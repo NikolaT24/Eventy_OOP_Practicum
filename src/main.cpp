b3e09b8e7bf821d49bde19084e87c7010a3cec5b
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-10 04:05:15 +0300

    Use EventySystem in main
    Refactored main function to use EventySystem instead of Client and Event.

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,8 @@
-#include <iostream>
-#include "Client.h"
-#include "Event.h"
+#include "EventySystem.h"
 
 int main() {
-    Client client(1, "nikola", "1234");
-    client.addBalance(100);
-
-    Event event(
-        1,
-        "CppSummit",
-        "2026-06-20",
-        "Sofia",
-        client.getId(),
-        EventType::Ticketed
-    );
-
-    event.setTicketInfo(25, 100);
-    event.publish();
-
-    int wantedTickets = 2;
-    double totalPrice = event.getTicketPrice() * wantedTickets;
-
-    std::cout << "Welcome to Eventy!" << std::endl;
-    std::cout << "Client: " << client.getUsername() << std::endl;
-    std::cout << "Balance: " << client.getBalance() << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "Event: " << event.getTitle() << std::endl;
-    std::cout << "Type: " << event.getTypeAsText() << std::endl;
-    std::cout << "Status: " << event.getStatusAsText() << std::endl;
-    std::cout << "Available tickets: " << event.getAvailableTickets() << std::endl;
-
-    std::cout << std::endl;
-
-    if (event.canSellTickets(wantedTickets) && client.withdraw(totalPrice)) {
-        event.sellTickets(wantedTickets);
-
-        std::cout << "Bought " << wantedTickets << " tickets successfully." << std::endl;
-        std::cout << "Remaining balance: " << client.getBalance() << std::endl;
-        std::cout << "Available tickets: " << event.getAvailableTickets() << std::endl;
-    }
-    else {
-        std::cout << "Could not buy tickets." << std::endl;
-    }
+    EventySystem system;
+    system.run();
 
     return 0;
 }
