078965c29b35b813ae9989ba0a6b893e3d4500fd
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-10 05:01:37 +0300

    Refactor Client class constructors and methods

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,4 +1,6 @@
 #include "Client.h"
+#include "Enums.h"
+#include <sstream>
 
 Client::Client() {
     this->id = 0;
@@ -8,12 +10,7 @@ Client::Client() {
     this->role = UserRole::Client;
 }
 
-Client::Client(
-    int id,
-    const std::string& username,
-    const std::string& password,
-    UserRole role
-) {
+Client::Client(int id, const std::string& username, const std::string& password, UserRole role) {
     this->id = id;
     this->username = username;
     this->password = password;
@@ -21,11 +18,19 @@ Client::Client(
     this->role = role;
 }
 
+Client::Client(int id, const std::string& username, const std::string& password, double balance, UserRole role) {
+    this->id = id;
+    this->username = username;
+    this->password = password;
+    this->balance = balance;
+    this->role = role;
+}
+
 int Client::getId() const {
     return this->id;
 }
 
-std::string Client::getUsername() const {
+const std::string& Client::getUsername() const {
     return this->username;
 }
 
@@ -37,6 +42,14 @@ double Client::getBalance() const {
     return this->balance;
 }
 
+UserRole Client::getRole() const {
+    return this->role;
+}
+
+bool Client::isAdmin() const {
+    return this->role == UserRole::Admin;
+}
+
 void Client::addBalance(double amount) {
     if (amount > 0) {
         this->balance += amount;
@@ -44,20 +57,21 @@ void Client::addBalance(double amount) {
 }
 
 bool Client::withdraw(double amount) {
-    if (amount <= 0) {
+    if (amount <= 0 || this->balance < amount) {
         return false;
     }
-    if (this->balance < amount) {
-        return false;
-    }
+
     this->balance -= amount;
     return true;
 }
 
-UserRole Client::getRole() const {
-    return this->role;
-}
-
-bool Client::isAdmin() const {
-    return this->role == UserRole::Admin;
+std::vector<std::string> Client::toRecord() const {
+    return {
+        "CLIENT",
+        std::to_string(this->id),
+        this->username,
+        this->password,
+        std::to_string(this->balance),
+        toString(this->role)
+    };
 }
