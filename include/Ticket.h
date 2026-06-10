8164d50331974fd26ce32aa7fc2120aaed95a439
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-10 04:51:44 +0300

    Enhance Ticket class with new attributes and methods

--- include/Ticket.h
+++ include/Ticket.h
@@ -2,6 +2,9 @@
 #define TICKET_H
 
 #include <string>
+#include <vector>
+#include "Enums.h"
+#include "SeatingPlan.h"
 
 class Ticket {
 private:
@@ -11,27 +14,24 @@ private:
     std::string eventTitle;
     int count;
     double totalPrice;
+    std::vector<Seat> seats;
+    ParticipationType participationType;
 
 public:
     Ticket();
-
-    Ticket(
-        int id,
-        int ownerId,
-        int eventId,
-        const std::string& eventTitle,
-        int count,
-        double totalPrice
-    );
+    Ticket(int id, int ownerId, int eventId, const std::string& eventTitle, int count, double totalPrice, const std::vector<Seat>& seats, ParticipationType participationType);
 
     int getId() const;
     int getOwnerId() const;
     int getEventId() const;
-    std::string getEventTitle() const;
+    const std::string& getEventTitle() const;
     int getCount() const;
     double getTotalPrice() const;
+    const std::vector<Seat>& getSeats() const;
+    ParticipationType getParticipationType() const;
 
     void print() const;
+    std::vector<std::string> toRecord() const;
 };
 
 #endif
