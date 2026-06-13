816196ed30bceaf00c5ebecffdb1146421fa6e61
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-14 00:51:43 +0300

    Update Ticket.h

--- include/Ticket.h
+++ include/Ticket.h
@@ -1,9 +1,8 @@
-#ifndef TICKET_H
-#define TICKET_H
+#pragma once
 
+#include <ostream>
 #include <string>
 #include <vector>
-#include "Enums.h"
 #include "SeatingPlan.h"
 
 class Ticket {
@@ -15,11 +14,9 @@ private:
     int count;
     double totalPrice;
     std::vector<Seat> seats;
-    ParticipationType participationType;
 
 public:
-    Ticket();
-    Ticket(int id, int ownerId, int eventId, const std::string& eventTitle, int count, double totalPrice, const std::vector<Seat>& seats, ParticipationType participationType);
+    Ticket(int id, int ownerId, int eventId, std::string eventTitle, int count, double totalPrice, std::vector<Seat> seats = {});
 
     int getId() const;
     int getOwnerId() const;
@@ -28,10 +25,7 @@ public:
     int getCount() const;
     double getTotalPrice() const;
     const std::vector<Seat>& getSeats() const;
-    ParticipationType getParticipationType() const;
-
-    void print() const;
     std::vector<std::string> toRecord() const;
-};
 
-#endif
+    friend std::ostream& operator<<(std::ostream& output, const Ticket& ticket);
+};
