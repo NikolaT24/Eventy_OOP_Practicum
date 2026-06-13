6c98084cd6153ad9e72f4b82204ecc713452a26b
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-14 00:54:24 +0300

    Create User.h

---
+++ include/User.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+#include "Enums.h"
+#include "Notification.h"
+
+class User {
+private:
+    int id;
+    std::string username;
+    std::string password;
+    std::vector<Notification> notifications;
+
+protected:
+    User(int id, std::string username, std::string password, std::vector<Notification> notifications = {});
+
+public:
+    virtual ~User() = default;
+
+    User(const User&) = default;
+    User& operator=(const User&) = default;
+    User(User&&) noexcept = default;
+    User& operator=(User&&) noexcept = default;
+
+    int getId() const;
+    const std::string& getUsername() const;
+    const std::string& getPassword() const;
+    bool checkPassword(const std::string& candidate) const;
+
+    const std::vector<Notification>& getNotifications() const;
+    std::vector<Notification>& getNotifications();
+    void addNotification(Notification notification);
+
+    virtual UserRole getRole() const = 0;
+    virtual std::unique_ptr<User> clone() const = 0;
+};
