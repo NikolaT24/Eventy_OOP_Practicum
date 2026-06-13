a7881579e4ca8ef99925bc530816a504825f85be
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-14 01:13:15 +0300

    Update EventyException.cpp

--- src/EventyException.cpp
+++ src/EventyException.cpp
@@ -1,1 +1,19 @@
+#include "EventyException.h"
 
+EventyException::EventyException(const std::string& message)
+    : std::runtime_error(message) {}
+
+ValidationException::ValidationException(const std::string& message)
+    : EventyException(message) {}
+
+AuthenticationException::AuthenticationException(const std::string& message)
+    : EventyException(message) {}
+
+AuthorizationException::AuthorizationException(const std::string& message)
+    : EventyException(message) {}
+
+NotFoundException::NotFoundException(const std::string& message)
+    : EventyException(message) {}
+
+InvalidStateException::InvalidStateException(const std::string& message)
+    : EventyException(message) {}
