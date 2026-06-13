af10886260459f4ee8a02051d40113a7c2486f4c
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-14 00:43:56 +0300

    Update Notification.h

--- include/Notification.h
+++ include/Notification.h
@@ -1,30 +1,24 @@
-#ifndef NOTIFICATION_H
-#define NOTIFICATION_H
+#pragma once
 
+#include <ostream>
 #include <string>
-#include <vector>
 
 class Notification {
 private:
     int id;
-    int receiverId;
     std::string message;
     std::string createdAt;
     bool read;
 
 public:
-    Notification();
-    Notification(int id, int receiverId, const std::string& message, const std::string& createdAt, bool read = false);
+    Notification(int id, std::string message, std::string createdAt, bool read = false);
 
     int getId() const;
-    int getReceiverId() const;
     const std::string& getMessage() const;
     const std::string& getCreatedAt() const;
     bool isRead() const;
 
     void markAsRead();
-    void print() const;
-    std::vector<std::string> toRecord() const;
-};
 
-#endif
+    friend std::ostream& operator<<(std::ostream& output, const Notification& notification);
+};
