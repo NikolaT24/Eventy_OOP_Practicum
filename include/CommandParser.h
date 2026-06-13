775fda923eedc75a3dc69abc0d37b88af8268acb
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-14 00:36:40 +0300

    Refactor CommandParser with new command structure

--- include/CommandParser.h
+++ include/CommandParser.h
@@ -1,17 +1,38 @@
-#ifndef COMMAND_PARSER_H
-#define COMMAND_PARSER_H
+#pragma once
 
+#include <functional>
+#include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 struct ParsedCommand {
     std::string name;
-    std::vector<std::string> args;
+    std::vector<std::string> arguments;
 };
 
-class CommandParser {
+class Command {
 public:
-    static ParsedCommand parse(const std::string& line);
+    virtual ~Command() = default;
+    virtual void execute(const std::vector<std::string>& arguments) = 0;
 };
 
-#endif
+class FunctionCommand final : public Command {
+private:
+    std::function<void(const std::vector<std::string>&)> action;
+
+public:
+    explicit FunctionCommand(std::function<void(const std::vector<std::string>&)> action);
+    void execute(const std::vector<std::string>& arguments) override;
+};
+
+class CommandRegistry {
+private:
+    std::unordered_map<std::string, std::unique_ptr<Command>> commands;
+
+public:
+    void add(const std::string& name, std::function<void(const std::vector<std::string>&)> action);
+    bool execute(const ParsedCommand& command) const;
+};
+
+ParsedCommand parseCommand(const std::string& line);
