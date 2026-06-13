dd7938cebbc7895408c31c54be0484f949e4e5ee
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-14 01:36:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
 #include "EventySystem.h"
 
 int main() {
-    EventySystem system;
-    system.run();
-
+    try {
+        EventySystem::instance().run();
+    } catch (const std::exception& error) {
+        std::cerr << "Fatal error: " << error.what() << '\n';
+        return 1;
+    }
     return 0;
 }
