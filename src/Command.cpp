52e7d1e5ac939844b760d74540613d8a91876c59
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-17 01:07:00 +0300

    Refactor CommandRegistry::add for better readability

--- src/Command.cpp
+++ src/Command.cpp
@@ -8,15 +8,15 @@ void FunctionCommand::execute(const std::vector<std::string>& arguments) {
     action(arguments);
 }
 
-void CommandRegistry::add(const std::string& name, std::function<void(const std::vector<std::string>&)> action) {
+void CommandRegistry::add(const std::string& name,
+                          std::function<void(const std::vector<std::string>&)> action) {
     commands[name] = std::make_unique<FunctionCommand>(std::move(action));
 }
 
 bool CommandRegistry::execute(const ParsedCommand& command) const {
     auto iterator = commands.find(command.name);
 
-    if (iterator == commands.end())
-        return false;
+    if (iterator == commands.end()) return false;
 
     iterator->second->execute(command.arguments);
     return true;
@@ -28,7 +28,9 @@ ParsedCommand parseCommand(const std::string& line) {
     input >> result.name;
 
     std::string argument;
-    while (input >> argument)
+    while (input >> argument) {
         result.arguments.push_back(argument);
+    }
+
     return result;
 }
