c19300ca428d4995d5a296d6e24d6c28bbc8efba
Nikola Topalov <117378752+NikolaT24@users.noreply.github.com>
2026-06-21 20:57:18 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+/**
+*
+* Solution to course project # 8
+* Object-Oriented Programming Practicum course
+* Faculty of Mathematics and Informatics of Sofia University
+* Summer semester 2025/2026
+*
+* @author Nikola Topalov
+* @idnumber 1MI0600248
+* @compiler VC
+*
+* <Eventy>
+*
+*/
+
 #include <iostream>
+
 #include "EventySystem.h"
 
 int main() {
